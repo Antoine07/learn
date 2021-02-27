810a9e06a95943ab1e5e931b876887e723352302
Antoine07 <antoine.lucsko@gmail.com>
2021-02-27 13:12:53 +0100

    constante url

--- assets/app/store/constants/actions.js
+++ assets/app/store/constants/actions.js
@@ -6,7 +6,7 @@ export const LOADING_LESSONS = 'LOADING_LESSONS';
 export const FILTER_LESSONS = 'FILTER_LESSONS';
 
 // url and KEY API
-export const URL_LAST_LESSONS = "http://127.0.0.1:8000/api/best-lessons";
-export const URL_API = "http://127.0.0.1:8000/api/";
+export const URL_LAST_LESSONS = "http://51.178.80.247/api/best-lessons";
+export const URL_API = "http://51.178.80.247/api/";
 
 export const MAX_BEST = 5;
